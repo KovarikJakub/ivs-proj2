ab545b278510ddfeb6550ae862c7b0f844d5dfd7
David Švancer <davidsvancer@Davids-MacBook-Air.local>
2024-03-20 22:58:56 +0100

    Add Empty math_lib structure

---
+++ src/lib/math_lib.cpp
@@ -0,0 +1,37 @@
+#include "math_lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+double Add(double x, double y)
+{
+    return 0;
+}
+double Sub(double x, double y)
+{
+    return 0;
+}
+double Div(double x, double y)
+{
+    return 0;
+}
+double Mul(double x, double y)
+{
+    return 0;
+}
+unsigned long long  Factorial(unsigned int x)
+{
+    return 0;
+}
+double Root(double x, double y)
+{
+    return 0;
+}
+double Pow(double x, unsigned int y)
+{
+    return 0;
+}
+double Abs(double x)
+{
+    return 0;
+}
+
---
+++ src/lib/math_lib.h
@@ -0,0 +1,13 @@
+#ifndef BASIC_MATH_H
+#define BASIC_MATH_H
+
+double Add(double x, double y);
+double Sub(double x, double y);
+double Div(double x, double y);
+double Mul(double x, double y);
+unsigned long long Factorial(unsigned int x);
+double Root(double x, double y);
+double Pow(double x, unsigned int y);
+double Abs(double x);
+
+#endif
